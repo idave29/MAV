6915fce763968a58d544a242d1d5bed3b90286e0
Kevin j13 G <kevinjavier.garcia@hotmail.com>
2021-09-17 17:47:25 -0500

    Relaciones de las clases Applicant y Intern

--- MAV/MAV.Web/Data/Entities/Applicant.cs
+++ MAV/MAV.Web/Data/Entities/Applicant.cs
@@ -1,8 +1,10 @@
 ﻿namespace MAV.Web.Data.Entities
 {
+    using System.Collections.Generic;
     public class Applicant : IEntity
     {
         public int Id { get; set; }
         public User User { get; set; }
+        public ICollection<Loan> Loans { get; set; }
     }
 }
--- MAV/MAV.Web/Data/Entities/Loan.cs
+++ MAV/MAV.Web/Data/Entities/Loan.cs
@@ -1,7 +1,12 @@
 ﻿namespace MAV.Web.Data.Entities
 {
+    using System.Collections.Generic;
     public class Loan
     {
         public int Id { get; set; }
+        public Applicant Applicant { get; set; }
+        public Intern Intern { get; set; }
+        public ICollection<LoanDetail> LoanDetails { set; get; }
+
     }
 }
