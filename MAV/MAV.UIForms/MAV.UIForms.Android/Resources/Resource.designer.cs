25d79cea5d380ca50ea3d008aa27053009d2030d
David Antonio Hernández Alvarez <david.hdezalv29@gmail.com>
2022-03-24 18:09:18 -0600

    APK de la aplicación

--- MAV/MAV.UIForms/MAV.UIForms.Android/Resources/Resource.designer.cs
+++ MAV/MAV.UIForms/MAV.UIForms.Android/Resources/Resource.designer.cs
@@ -8364,88 +8364,91 @@ namespace MAV.UIForms.Droid
 			public const int internt = 2131165294;
 			
 			// aapt resource value: 0x7F07006F
-			public const int loans = 2131165295;
+			public const int libretaLauncher = 2131165295;
 			
 			// aapt resource value: 0x7F070070
-			public const int loansDetails = 2131165296;
+			public const int loans = 2131165296;
 			
 			// aapt resource value: 0x7F070071
-			public const int logoumad = 2131165297;
+			public const int loansDetails = 2131165297;
 			
 			// aapt resource value: 0x7F070072
-			public const int material = 2131165298;
+			public const int logoumad = 2131165298;
 			
 			// aapt resource value: 0x7F070073
-			public const int materialType = 2131165299;
+			public const int material = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int mtrl_snackbar_background = 2131165300;
+			public const int materialType = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int mtrl_tabs_default_indicator = 2131165301;
+			public const int mtrl_snackbar_background = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int navigation_empty_icon = 2131165302;
+			public const int mtrl_tabs_default_indicator = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int notification_action_background = 2131165303;
+			public const int navigation_empty_icon = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int notification_bg = 2131165304;
+			public const int notification_action_background = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int notification_bg_low = 2131165305;
+			public const int notification_bg = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int notification_bg_low_normal = 2131165306;
+			public const int notification_bg_low = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int notification_bg_low_pressed = 2131165307;
+			public const int notification_bg_low_normal = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int notification_bg_normal = 2131165308;
+			public const int notification_bg_low_pressed = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int notification_bg_normal_pressed = 2131165309;
+			public const int notification_bg_normal = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int notification_icon_background = 2131165310;
+			public const int notification_bg_normal_pressed = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int notification_template_icon_bg = 2131165311;
+			public const int notification_icon_background = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int notification_template_icon_low_bg = 2131165312;
+			public const int notification_template_icon_bg = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int notification_tile_bg = 2131165313;
+			public const int notification_template_icon_low_bg = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int notify_panel_notification_icon_bg = 2131165314;
+			public const int notification_tile_bg = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int owner = 2131165315;
+			public const int notify_panel_notification_icon_bg = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int plecaumad = 2131165316;
+			public const int owner = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int select = 2131165317;
+			public const int plecaumad = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int setup = 2131165318;
+			public const int select = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int SplashUMAD = 2131165319;
+			public const int setup = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int status = 2131165320;
+			public const int SplashUMAD = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int tooltip_frame_dark = 2131165321;
+			public const int status = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int tooltip_frame_light = 2131165322;
+			public const int tooltip_frame_dark = 2131165322;
+			
+			// aapt resource value: 0x7F07008B
+			public const int tooltip_frame_light = 2131165323;
 			
 			static Drawable()
 			{
@@ -9411,6 +9414,15 @@ namespace MAV.UIForms.Droid
 			
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int libretaLauncher = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int libretalauncher_adaptive_back = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int libretalauncher_adaptive_fore = 2131492869;
 			
 			static Mipmap()
 			{
